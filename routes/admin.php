6ae0050399e92a7406df4eb3868eaa886e9960ea
ilhamsahid <ilhamsahit988@gmail.com>
2025-08-10 08:37:58 +0700

    feat: menambah route admin

--- routes/admin.php
+++ routes/admin.php
@@ -2,5 +2,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth.custom', 'role:admin'])->group(function () {
-    Route::view('ekstrasmexa/admin/dashboard', 'admin.dashboard');
+    Route::get('/ekstrasmexa/admin/{any}', function () {
+        return view('admin.main'); // return blade utama yang isinya JS SPA kamu
+    })->where('any', '.*');
 });
