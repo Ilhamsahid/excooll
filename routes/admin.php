4ca05a10fc51cbdd5475fd81da64edd0d53878f3
ilhamsahid <ilhamsahit988@gmail.com>
2025-08-10 09:38:08 +0700

    chore: mengapus yang tidak perlu

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,11 @@
 <?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\EkskulController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth.custom', 'role:admin'])->group(function () {
-    Route::get('/ekstrasmexa/admin/{any}', function () {
-        return view('admin.main'); // return blade utama yang isinya JS SPA kamu
-    })->where('any', '.*');
+    Route::get('/ekstrasmexa/admin/{any}', [AdminController::class, 'index'])->where('any', '.*');
+
+    Route::get('ekskul/api', [EkskulController::class, 'getAllEkskulWithRelation']);
 });
--- routes/guest.php
+++ routes/guest.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EkskulController;
 use App\Http\Controllers\GuestController;
 use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
